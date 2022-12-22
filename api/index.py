501ee24820950afc33dab0417fc80221b2761e18
ATACOM <120112713+ATACOM@users.noreply.github.com>
2022-12-22 21:22:15 +0200

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,1 +1,46 @@
+from flask import Flask
+app = Flask(__name__)
 
+#Main tasks
+
+@app.route('/welcome')
+def welcome():
+    return 'welcome'
+
+@app.route('/welcome/home')
+def welcome_home():
+    return 'welcome home'
+
+@app.route('/welcome/back')
+def welcome_back():
+    return 'welcome back'  
+
+# For fun
+
+# 1st page
+
+@app.route('/')
+def root():
+    return """
+    <h3>1st task links</h3>
+    <p><b>Please use links below or type them by yourself:</b></p>
+    <ul>
+    <li><a href="https://1uzdevums.vercel.app/welcome">welcome</a></li>
+    <li><a href="https://1uzdevums.vercel.app/welcome/home">welcome/home</a></li>
+    <li><a href="https://1uzdevums.vercel.app/welcome/back">welcome/back</a></li>
+    </ul>
+    """
+
+#In case, if adress end with "/"
+
+@app.route('/welcome/')
+def welcome2():
+    return 'welcome'
+
+@app.route('/welcome/home/')
+def welcome_home2():
+    return 'welcome home'
+
+@app.route('/welcome/back/')
+def welcome_back2():
+    return 'welcome back'
